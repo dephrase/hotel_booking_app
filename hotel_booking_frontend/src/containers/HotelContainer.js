58ee2a4b92783b7d89a73d3dfab8323fcc4b5c85
Dephrase <davidjf123@gmail.com>
2021-08-16 12:11:28 +0100

    Getting hotels from search results

--- hotel_booking_frontend/src/containers/HotelContainer.js
+++ hotel_booking_frontend/src/containers/HotelContainer.js
@@ -5,8 +5,8 @@ const HotelContainer = ({bookingInfo}) => {
     const [hotels, setHotels] = useState(null);
 
     useEffect(() => {
-        if(bookingInfo){
-            fetch(`https://hotels4.p.rapidapi.com/properties/list?destinationId=${bookingInfo.CityCode}&pageNumber=1&pageSize=25&checkIn=${Date.parse(bookingInfo.Checkin)}&checkOut=${Date.parse(bookingInfo.checkout)}&adults1=${bookingInfo.Adults}&sortOrder=PRICE&locale=en_US&currency=GBP`, {
+        if(bookingInfo){        
+            fetch(`https://hotels4.p.rapidapi.com/properties/list?destinationId=${bookingInfo.CityCode}&pageNumber=1&pageSize=25&checkIn=${bookingInfo.Checkin}&checkOut=${bookingInfo.Checkout}&adults1=${bookingInfo.Adults}&sortOrder=PRICE&locale=en_US&currency=GBP`, {
 	            "method": "GET",
 	            "headers": {
 		            "x-rapidapi-key": "7f1f9f12f0mshb61724ebaadc332p1f6658jsnd12fd2c73285",
@@ -18,11 +18,19 @@ const HotelContainer = ({bookingInfo}) => {
         }
     }, [bookingInfo])
 
+    function renderHotels(){
+        if(hotels){
+            for(let hotel of hotels.data.body.searchResults.results){
+                console.log(hotel.name)
+            }
+        }
+    }
+
     return (
         <>
         <div className = "hotel_container">
             <p>Hotel Container</p>
-            {hotels ? console.log(hotels) && console.log(Date.parse(bookingInfo.Checkin)) : null}
+            {hotels ? renderHotels() : null}
         </div>
         </>
     )
