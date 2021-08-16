272c634a02b9ecb30c90fc4a093a54559f3763ea
Dephrase <davidjf123@gmail.com>
2021-08-16 13:51:32 +0100

    Adding hotel results to divs and displaying them on the screen

--- hotel_booking_frontend/src/containers/HotelContainer.js
+++ hotel_booking_frontend/src/containers/HotelContainer.js
@@ -19,10 +19,26 @@ const HotelContainer = ({bookingInfo}) => {
     }, [bookingInfo])
 
     function renderHotels(){
+        let hotelDivs = [];
         if(hotels){
+            let index = 0;
+            console.log(hotels)
             for(let hotel of hotels.data.body.searchResults.results){
-                console.log(hotel.name)
+                let hotelDiv = <div key={index} className="hotel_div">
+                    <h4>{hotel.name}</h4>
+                    <img src={hotel.optimizedThumbUrls.srpDesktop} alt="Hotel Thumbnail"/>
+                    <h6>{hotel.neighbourhood}</h6>
+                    <p>{hotel.address.streetAddress}</p>
+                    <p>{hotel.landmarks[0].distance} from {hotel.landmarks[0].label}</p>
+                    <p>{hotel.starRating} stars</p>
+                    <p>{hotel.ratePlan.price.fullyBundledPricePerStay}</p>
+                    <p>{hotel.ratePlan.price.info}: {hotel.ratePlan.price.current}</p>
+                    <p>destinationID: {hotel.destinationId}</p>
+                </div>
+                hotelDivs.push(hotelDiv)
+                index++;
             }
+            return hotelDivs;
         }
     }
 
