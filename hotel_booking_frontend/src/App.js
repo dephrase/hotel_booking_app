46068f954aa21d7174ab221bb51644a8b75264e8
Dephrase <davidjf123@gmail.com>
2021-08-18 12:21:36 +0100

    Getting a customer from the backend and putting it in state in the front end

--- hotel_booking_frontend/src/App.js
+++ hotel_booking_frontend/src/App.js
@@ -3,15 +3,28 @@ import MainContainer from './containers/MainContainer';
 import HotelInfo from './components/HotelInfo';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom"; 
 import NavBar from './components/NavBar';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import Confirmation from './components/Confirmation';
+import Request from './helpers/request';
 
 function App() {
 
   const [bookingInfo, setBookingInfo] = useState(null)
   const [hotelDetails, setHotel] = useState(null)
   const [hotelFinalDetails, setHotelFinalDetails] = useState(null)
-  
+  const [user, setUser] = useState(null)
+
+  const requestUser = function(){
+    const request = new Request();
+    const userPromise = request.get('/api/customers')
+    .then((data) => {
+      setUser(data[0]);
+    })
+  }
+
+  useEffect(() => {
+    requestUser()
+  }, [])
 
   return (
     <Router>
